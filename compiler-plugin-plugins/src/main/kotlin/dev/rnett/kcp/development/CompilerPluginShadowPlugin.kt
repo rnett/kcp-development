108d035953d4a1b2c93f8ca2d48f5e82a6ae56be
Ryan Nett <JNett96@gmail.com>
2025-11-02 19:15:10 -0800

    Poison the non-shadow variant of the compiler plugin

--- compiler-plugin-plugins/src/main/kotlin/dev/rnett/kcp/development/CompilerPluginShadowPlugin.kt
+++ compiler-plugin-plugins/src/main/kotlin/dev/rnett/kcp/development/CompilerPluginShadowPlugin.kt
@@ -3,6 +3,8 @@ package dev.rnett.kcp.development
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.attributes.Usage
+import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.named
 
 public class CompilerPluginShadowPlugin : Plugin<Project> {
@@ -10,15 +12,23 @@ public class CompilerPluginShadowPlugin : Plugin<Project> {
         pluginManager.apply(CompilerPluginBasePlugin::class.java)
         pluginManager.apply("com.gradleup.shadow")
 
+        // poison the non-shadow variant
+        configurations.named("runtimeElements") {
+            attributes {
+                attribute(Usage.USAGE_ATTRIBUTE, objects.named("disabled-usage"))
+            }
+        }
+
         pluginManager.withPlugin("com.gradleup.shadow") {
             tasks.named<ShadowJar>("shadowJar") {
                 archiveClassifier.set("")
                 dependencies {
                     exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib"))
+                    exclude(dependency("org.jetbrains.kotlin:kotlin-compiler"))
                 }
             }
-            tasks.named("jar") {
-                enabled = false
+            tasks.named<Jar>("jar") {
+                archiveClassifier.set("ignored")
             }
         }
     }
