3a9a545b59419815f67dc38eed904878cccfb3af
Ryan Nett <JNett96@gmail.com>
2025-12-06 15:23:06 -0800

    Try turning off CC for the integration tests

--- integration-test/settings.gradle.kts
+++ integration-test/settings.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
-        gradlePluginPortal()
     }
     versionCatalogs {
         register("libs") { from(files("../gradle/libs.versions.toml")) }
